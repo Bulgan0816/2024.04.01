f88f01b17a241188ec80ac0b964d4f57dc86d334
Bulgan0816 <bulgaa05281631@gmail.com>
2024-04-01 21:27:31 +0800

    first commit

---
+++ resources/views/layouts/inc/admin/sidebar.blade.php
@@ -0,0 +1,10 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item">
+            <a href="nav-link" href="{{url('admin/dashboard')}}">
+                <i class="mdi mdi-home menu-icon"></i>
+                <span class="menu-title"> DASHBOARD</span>
+            </a>
+        </li>
+    </ul>
+</nav>
